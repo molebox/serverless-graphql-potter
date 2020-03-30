fcf5d931b0dded434d5853e403287d2259d89fbc
HAINES-DESKTOP\richa <richardkhaines@hotmail.co.uk>
2020-03-30 21:34:24 +0200

    none

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,25 @@
 import React from 'react'
-import { graphql } from 'gatsby'
+// import { graphql } from 'gatsby'
 
-export default ({data}) => {
-    console.log({data});
+export default () => {
+    // console.log({data});
     return (
         <div>
             <h1>Potter Site</h1>
-            {data.potterAPI.allCharacters.map((char) => (
+            {/* {data.potterAPI.allCharacters.map((char) => (
                 <p>{char.name}</p>
-            ))}
+            ))} */}
         </div>
     )
 }
 
-export const query = graphql`
-    {
-        potterAPI {
-            allCharacters {
-                name
-            }
-        }
+// export const query = graphql`
+//     {
+//         potterAPI {
+//             allCharacters {
+//                 name
+//             }
+//         }
 
-    }
-`;
+//     }
+// `;
